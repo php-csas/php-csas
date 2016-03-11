2eb488727228cf7521c07c99a9a9ba109bb20b9c
Joseph Connor <josephc346@gmail.com>
2016-03-11 21:06:34 +0000

    Add php file to test html parsing in different contexsts

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+	$url       = "http://example.com";
+    $js        = "alert('hello world');";
+
+    echo "<a href='";
+	echo url;
+    echo "'>'";
+
+    echo "<script>";
+	echo js;
+    echo "</script>";
+?>
+
+Just pure HTML 
