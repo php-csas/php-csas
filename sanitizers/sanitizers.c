50113723b6fa547e842a4286b0f26e6bc85905ba
davpcunn <davpcunn@utk.edu>
2016-04-01 22:10:10 +0000

    added license

--- sanitizers/sanitizers.c
+++ sanitizers/sanitizers.c
@@ -1,3 +1,37 @@
+/*
+  +----------------------------------------------------------------------+
+  | Taint                                                                |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2012-2015 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:  Xinchen Hui    <laruence@php.net>                           |
+  +----------------------------------------------------------------------+
+
+  +----------------------------------------------------------------------+
+  | CSAS                                                                 |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2012-2015 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:  Matt Van Gundy    <mvangund@cisco.com>                      |
+  +----------------------------------------------------------------------+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
